2c4ed38262e04b2e49c0540a5491a3234d943f3d
V Gupta <vivgupta@me.com>
2023-07-08 16:08:17 +0100

    Add javascript refactoring

--- js/app.js
+++ js/app.js
@@ -33,8 +33,13 @@ const moon = `<svg id="modeSVG" xmlns="http://www.w3.org/2000/svg" width="16" he
             <path class="moon" d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
             </svg>`;
 
+const nodeColors = [mode, brand, viewWork, portWrap, aboutH2, copyright];
+const nodeBackgroundColors = [body, navBackground, cover, portWrap];
+const nodesToLoop = [topNav, coverH2, aboutP, footNav];
+
 // Callback function to run everytime the visibility of an element changes. 
 // It will loop over every element with th class 'hidden' and change it to show to trigger animation once its visible in the viewport.
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -44,33 +49,70 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
+// Helper forEach function
+
+function loopNodes(nodes, c) {
+    return nodes.forEach(node => node.style.color = c);
+}
+
+// Function to replace mode SVG
+
+function replaceSVG(modetypeSVG) {
+    let svg = document.querySelector('#modeSVG');
+    mode.removeChild(svg);
+    mode.insertAdjacentHTML('afterbegin', modetypeSVG);
+}
+
+// Helper function to change text colors on mode change
+
+function changeTextColor(c) {
+    nodeColors.forEach(node => node.style.color = c);
+    nodesToLoop.forEach(arr => loopNodes(arr, c));
+    footWrap.style.borderTop = `1px solid ${c}`;
+    linkedin.style.fill = c;
+    github.style.fill = c;
+}
+
+// Dark mode function
+
+function darkMode() {
+    replaceSVG(moon);
+    changeTextColor(colorSecondary);
+    nodeBackgroundColors.forEach(node => node.style.backgroundColor = colorPrimary);
+    h1.style.color = '#FFFFFF';
+    cards.forEach(card => card.style.backgroundColor = colorTeritary);
+}
+
+// Light mode function
+
+function lightMode() {
+    replaceSVG(sun);
+    changeTextColor(colorPrimary);
+    nodeBackgroundColors.forEach(node => node.style.backgroundColor = colorSecondary);
+    h1.style.color = '#000000';
+    viewWork.style.color = colorTeritary;
+    cards.forEach(card => {
+        card.style.color = colorSecondary;
+        card.style.backgroundColor = colorPrimary;
+    });
+}
+
 // Function to retrieve mode that user was in
 
 function retrieveSettings() {
     setting = localStorage.getItem('mode');
     if (setting === 'dark') {
-        let svg = document.querySelector('#modeSVG');
-        mode.removeChild(svg);
-        mode.insertAdjacentHTML('afterbegin', moon);
-        mode.style.color = colorSecondary;
-        body.style.backgroundColor = colorPrimary;
-        topNav.forEach(nav => nav.style.color = colorSecondary);
-        navBackground.style.backgroundColor = colorPrimary;
-        brand.style.color = colorSecondary;
-        cover.style.backgroundColor = colorPrimary;
-        h1.style.color = '#FFFFFF';
-        coverH2.forEach(p => p.style.color = colorSecondary);
-        viewWork.style.color = colorSecondary;
-        portWrap.style.color = colorSecondary;
-        portWrap.style.backgroundColor = colorPrimary;
-        cards.forEach(card => card.style.backgroundColor = colorTeritary);
-        aboutH2.style.color = colorSecondary;
-        aboutP.forEach(p => p.style.color = colorSecondary);
-        footNav.forEach(nav => nav.style.color = colorSecondary);
-        footWrap.style.borderTop = `1px solid ${colorSecondary}`;
-        linkedin.style.fill = colorSecondary;
-        github.style.fill = colorSecondary;
-        copyright.style.color = colorSecondary;
+        darkMode();
+    }
+}
+
+// Helper function to return SVGs to original state on 'mouseout' event
+
+function originalState(node) {
+    if (localStorage.getItem('mode') === 'dark'){
+        node.style.fill = colorSecondary;    
+    } else {
+        node.style.fill = colorPrimary; 
     }
 }
 
@@ -80,19 +122,11 @@ gLink.addEventListener('mouseover', () => {github.style.fill = '#6e5494'});
 lLink.addEventListener('mouseover', () => {linkedin.style.fill = '#0072b1'});
 
 gLink.addEventListener('mouseout', () => {
-    if (localStorage.getItem('mode') === 'dark'){
-        github.style.fill = colorSecondary;    
-    } else {
-        github.style.fill = colorPrimary; 
-    }   
+    originalState(github);  
 });
 
 lLink.addEventListener('mouseout', () => {
-    if (localStorage.getItem('mode') === 'dark'){
-        linkedin.style.fill = colorSecondary;    
-    } else {
-        linkedin.style.fill = colorPrimary; 
-    }   
+    originalState(linkedin);  
 });
 
 // Event listener for dark and light mode
@@ -107,54 +141,9 @@ mode.addEventListener('click', (e) => {
             localStorage.setItem('mode', 'light');
         }    
     if (localStorage.getItem('mode') === 'dark') {
-        let svg = document.querySelector('#modeSVG');
-        mode.removeChild(svg);
-        mode.insertAdjacentHTML('afterbegin', moon);
-        mode.style.color = colorSecondary;
-        body.style.backgroundColor = colorPrimary;
-        topNav.forEach(nav => nav.style.color = colorSecondary);
-        navBackground.style.backgroundColor = colorPrimary;
-        brand.style.color = colorSecondary;
-        cover.style.backgroundColor = colorPrimary;
-        h1.style.color = '#FFFFFF';
-        coverH2.forEach(p => p.style.color = colorSecondary);
-        viewWork.style.color = colorSecondary;
-        portWrap.style.color = colorSecondary;
-        portWrap.style.backgroundColor = colorPrimary;
-        cards.forEach(card => card.style.backgroundColor = colorTeritary);
-        aboutH2.style.color = colorSecondary;
-        aboutP.forEach(p => p.style.color = colorSecondary);
-        footNav.forEach(nav => nav.style.color = colorSecondary);
-        footWrap.style.borderTop = `1px solid ${colorSecondary}`;
-        linkedin.style.fill = colorSecondary;
-        github.style.fill = colorSecondary;
-        copyright.style.color = colorSecondary;
+        darkMode();
     } else {
-        let svg = document.querySelector('#modeSVG');
-        mode.removeChild(svg);
-        mode.insertAdjacentHTML('afterbegin', sun);
-        mode.style.color = colorPrimary;
-        body.style.backgroundColor = colorSecondary;
-        topNav.forEach(nav => nav.style.color = colorPrimary);
-        navBackground.style.backgroundColor = colorSecondary;
-        brand.style.color = colorPrimary;
-        cover.style.backgroundColor = colorSecondary;
-        h1.style.color = '#000000';
-        coverH2.forEach(p => p.style.color = colorTeritary);
-        viewWork.style.color = colorTeritary;
-        portWrap.style.color = colorPrimary;
-        portWrap.style.backgroundColor = colorSecondary;
-        cards.forEach(card => {
-            card.style.color = colorSecondary;
-            card.style.backgroundColor = colorPrimary;
-        });
-        aboutH2.style.color = colorPrimary;
-        aboutP.forEach(p => p.style.color = colorPrimary);
-        footNav.forEach(nav => nav.style.color = colorPrimary);
-        footWrap.style.borderTop = `1px solid ${colorPrimary}`;
-        linkedin.style.fill = colorPrimary;
-        github.style.fill = colorPrimary;
-        copyright.style.color = colorPrimary;
+        lightMode();
     }
     
 })
